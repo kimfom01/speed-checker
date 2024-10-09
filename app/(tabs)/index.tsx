e0157e30bd008f227d153c0b29ada588da3ffa7e
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-10-09 11:58:01 +0300

    Remove boiler plate, add initial algorithm

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,84 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { ScrollView, Text, View, Platform } from "react-native";
+import { Accelerometer } from "expo-sensors";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+const TimeInSeconds = 2.0;
+const Gravity = 9.8;
+
+interface Coords {
+  x: number;
+  y: number;
+  z: number;
+}
 
 export default function HomeScreen() {
+  const [accelerations, setAccelerations] = useState<Coords>({
+    x: 0,
+    y: 0,
+    z: 0,
+  });
+
+  useEffect(() => {
+    Accelerometer.addListener((accelerometerData) =>
+      setAccelerations(accelerometerData)
+    );
+    Accelerometer.setUpdateInterval(TimeInSeconds * 1000);
+
+    return () => {
+      Accelerometer.removeAllListeners();
+    };
+  }, []);
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <ScrollView style={{ padding: 10, paddingTop: 50 }}>
+      <View
+        style={{ display: "flex", justifyContent: "center", width: "100%" }}
+      >
+        <Text style={{ color: "white", fontSize: 50, fontWeight: "bold" }}>
+          Speed Checker <Text style={{ fontSize: 20 }}>v0.1</Text>
+        </Text>
+        <Text style={{ color: "white" }}>
+          Currently running on{" "}
+          <Text style={{ fontWeight: "bold" }}>IOS {Platform.Version}</Text>
+        </Text>
+      </View>
+      <SpeedCalculator accelerations={accelerations} />
+    </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+interface SpeedCalculatorProps {
+  accelerations: Coords;
+}
+
+const SpeedCalculator = ({ accelerations }: SpeedCalculatorProps) => {
+  const [speeds, setSpeeds] = useState<Coords>({
+    x: 0,
+    y: 0,
+    z: 0,
+  });
+
+  useEffect(() => {
+    setSpeeds((oldSpeeds) => ({
+      x: oldSpeeds.x + accelerations.x * TimeInSeconds,
+      y: oldSpeeds.y + accelerations.y * TimeInSeconds,
+      z: oldSpeeds.y + accelerations.z * TimeInSeconds,
+    }));
+  }, [accelerations]);
+
+  const aggregatedSpeed = (speeds: Coords) => {
+    return (
+      Math.sqrt(
+        Math.pow(speeds.x, 2) + Math.pow(speeds.y, 2) + Math.pow(speeds.z, 2)
+      ) - Gravity
+    );
+  };
+
+  return (
+    <View style={{ marginTop: 40 }}>
+      <Text style={{ color: "white", fontSize: 40 }}>
+        Current Speed: {aggregatedSpeed(speeds).toFixed(2)} m/s
+      </Text>
+    </View>
+  );
+};
